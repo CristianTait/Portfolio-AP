cd093187d209137c31792a8f012011ae1bd89cad
Cristian <90878440+CristianTait@users.noreply.github.com>
2023-05-10 01:54:41 -0300

    Credenciales Google Firebase

---
+++ PortafolioFrontEnd/src/app/environments/firebase.config.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAKDMlznkO6dPu1yYO2ANjoRNrRohIoTOg",
+  authDomain: "yoprogramoprogramo.firebaseapp.com",
+  databaseURL: "https://yoprogramoprogramo-default-rtdb.firebaseio.com",
+  projectId: "yoprogramoprogramo",
+  storageBucket: "yoprogramoprogramo.appspot.com",
+  messagingSenderId: "254368116986",
+  appId: "1:254368116986:web:0ed0ed80ba7c8e9dc15943"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default firebaseConfig;
